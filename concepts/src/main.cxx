a297de2ec93a4e5696a1e168547c616a3b8b4a1e
Markus Rathgeb <maggu2810@gmail.com>
2024-07-07 17:47:20 +0200

    add concepts
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

---
+++ concepts/src/main.cxx
@@ -0,0 +1,42 @@
+#include <concepts>
+
+template<typename T>
+concept foo1 = requires(T v)
+{
+    { v.f1() } -> std::same_as<bool>;
+};
+
+template<typename T>
+concept foo2 = requires(T v)
+{
+    v.f2(std::declval<int>());
+};
+
+template<typename T>
+concept foo3 = requires(T v, int i)
+{
+    { v.f3(i) } -> std::same_as<void>;
+};
+
+class Test {
+public:
+    bool f1() { return false; }
+
+    void f2(int i) {}
+
+    void f3(int i) {}
+};
+
+static_assert(foo1<Test>);
+static_assert(foo2<Test>);
+static_assert(foo3<Test>);
+
+template<typename T>
+void test(T arg) requires foo1<T> && foo2<T> {
+    arg.f1();
+    arg.f2(0);
+}
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
